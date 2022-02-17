e87dd4c5ede149081c62ba768a34401bbd3f0c64
hacker-DOM <dteiml@gmail.com>
2022-02-17 16:44:20 +0300

    :art: Code formatting

--- contracts/QD.sol
+++ contracts/QD.sol
@@ -7,24 +7,26 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 contract QD is Ownable, ERC20 {
-
     using SafeERC20 for ERC20;
 
-    uint constant internal _QD_DECIMALS   = 24;
-    uint constant internal _USDT_DECIMALS = 6;
-    uint constant public AUCTION_LENGTH   = 42 days;
-    uint constant public start_price      = 12;
-    uint constant public final_price      = 94;
-
-    address immutable public usdt;
-    uint    immutable public auction_start;
+    // Constants
+    uint constant internal _QD_DECIMALS   = 24     ;
+    uint constant internal _USDT_DECIMALS = 6      ;
+    uint constant public   AUCTION_LENGTH = 42 days;
 
     // In cents
+    uint constant public start_price      = 12     ;
+    uint constant public final_price      = 94     ;
 
-    constructor(
-        address _usdt
-    ) ERC20("QuiD", "QD") {
-        // TODO: Change
+    // Set in constructor and never changed
+    address immutable public usdt                  ;
+    uint    immutable public auction_start         ;
+
+
+    constructor(RC20 _usdt) ERC20("QuiD", "QD") {
+        // Data validation
+        require(_usdt.symbol() == "USDT", "QD: CONSTRUCTOR_R1");
+
         usdt = _usdt;
 
         // Auction starts on deployment
@@ -33,19 +35,23 @@ contract QD is Ownable, ERC20 {
         // By default, owner is set to msg.sender
     }
 
-    function mint(uint qd_amount) external {
+    function mint(uint qd_amount) external returns (uint cost_in_usdt) {
+        // Data validation
         require(qd_amount > 0, "QD: MINT_R1");
         require(block.timestamp < auction_start + AUCTION_LENGTH, "QD: MINT_R2");
 
+        // Optimistic mint
         _mint(_msgSender(), qd_amount);
 
-        uint cost_in_usdt = qd_amount_to_usdt_amount(qd_amount, block.timestamp);
+        // Calculate cost in USDT based on current price
+        cost_in_usdt = qd_amount_to_usdt_amount(qd_amount, block.timestamp);
 
-        ERC20(usdt).safeTransferFrom(_msgSender(), address(this), cost_in_usdt);
+        // Will revert on failure (namely insufficient allowance)
+        usdt.safeTransferFrom(_msgSender(), address(this), cost_in_usdt);
     }
 
     function withdraw(uint amount) external onlyOwner {
-        ERC20(usdt).safeTransfer(owner(), amount);
+        usdt.safeTransfer(owner(), amount);
     }
 
     function decimals() public view override(ERC20) returns (uint8) {
@@ -56,6 +62,8 @@ contract QD is Ownable, ERC20 {
         uint qd_amount,
         uint block_timestamp
     ) public view returns (uint usdt_amount) {
+        // Do data validation just in case
+        require(block_timestamp < auction_start + AUCTION_LENGTH, "QD: QD_AMOUNT_TO_USDT_AMOUNT_R1")
         uint time_elapsed = block_timestamp - auction_start;
 
         // price = ((now - auction_start) // auction_length) * (final_price - start_price) + start_price
