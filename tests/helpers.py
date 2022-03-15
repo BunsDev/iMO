cda36db1987ed9ba1d673fedbc31117eb32ad6f6
Dominik Teiml <dteiml@gmail.com>
2022-03-15 16:21:41 +0100

    :bug: fix off_by_atmost_1bp

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,5 +2,5 @@ def off_by_atmost_1(x: int, y: int) -> bool:
     return abs(x - y) <= 1
 
 def off_by_atmost_1bp(x: int, y: int) -> bool:
-    return abs(x / y - 1) <= 0.01
+    return abs(x / y - 1) <= 0.0001
     
